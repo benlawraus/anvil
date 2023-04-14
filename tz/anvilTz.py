842227de167ec1a9a3933b1e863977c6c1ad4748
ben <benlawraus@gmail.com>
2023-04-14 09:01:19 -0700

    filled in anvilTz

--- tz/anvilTz.py
+++ tz/anvilTz.py
@@ -1,16 +1,29 @@
-from dataclasses import dataclass
+from datetime import datetime, timedelta, timezone, tzinfo
+
+UTC = timezone.utc
 
 
-@dataclass
-class tzoffset():
-    pass
+class tzoffset(tzinfo):
+    def __init__(self, seconds=0, minutes=0, hours=0):
+        self._offset = timedelta(hours=hours, minutes=minutes, seconds=0)
+
+    def utcoffset(self, dt):
+        return self._offset
+
+    def tzname(self, dt):
+        return f"UTC{self._offset:+}"
+
+    def dst(self, dt):
+        return timedelta(0)
 
 
-@dataclass
 class tzlocal(tzoffset):
-    pass
+    """Use the local timezone of the browser"""
+
+    def __init__(self):
+        super().__init__(datetime.now().second, datetime.now().minute, datetime.now().hour)
 
 
-@dataclass
 class tzutc(tzoffset):
-    pass
+    def __init__(self):
+        super().__init__(datetime.utcnow().second, datetime.utcnow().minute, datetime.utcnow().hour)
